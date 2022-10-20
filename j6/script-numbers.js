94f74f0d97afa156cf6cebe6576513e56a75a05c
Talmage Bird <talmagebird@gmail.com>
2022-10-20 17:43:34 +0000

    PublicAWSALL

---
+++ j6/script-numbers.js
@@ -0,0 +1,38 @@
+function onClick(e) {
+  e.preventDefault();
+  // get form values
+  let number = document.getElementById('number').value;
+  let s = document.getElementById('selector');
+  
+  let type = s.options[s.selectedIndex].value;
+
+  // check if number is empty
+  if (number === "") {
+    number = "random";
+  }
+  
+  // setup URL
+  // The cslone.byu.edu forwards to the numbers API but converts it to https
+  let url = "https://cors-anywhere.herokuapp.com/http://numbersapi.com/" + number + "/" + type + "?json";
+  /*global fetch*/
+  fetch(url)
+    .then(function(response) {
+      // make sure the request was successful
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Numbers API service: " + response.statusText
+        }
+      }
+      return response.json();
+
+    }).then(function(json) {
+      // update DOM with response
+      updateResult(json.text);
+    });
+}
+      
+function updateResult(info) {
+  document.getElementById('result').textContent = info;
+}
+
+document.getElementById('woo').addEventListener('click', onClick);
